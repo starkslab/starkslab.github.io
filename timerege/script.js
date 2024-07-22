5a974bcc044f8b85ec5da30dce62bb2ac25a409c
unatteksen <unatteksen@gmail.com>
2024-07-22 13:18:36 +0300

    add sec

--- timerege/script.js
+++ timerege/script.js
@@ -6,10 +6,14 @@ function countdown() {
     const years = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 365));
     const days = Math.floor((timeDifference % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
     document.getElementById("years").innerText = years;
     document.getElementById("days").innerText = days;
     document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
 }
 
 setInterval(countdown, 1000);
